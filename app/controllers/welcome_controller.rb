ad2163e1c895fdc7b43e2a0006ae0f39d9184eff
Peter Francisco Balanesi <balanesipf@gmail.com>
2014-03-30 10:56:31 -0400

    gallerific

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -31,7 +31,19 @@ class WelcomeController < ApplicationController
   
   def showitem
     @item = CollectionItem.find(params[:id])
+    @thumb1 = @item.imageUrl1[0..-5]
+    @thumb1 = @thumb1 + "thumb.jpg"
+    @thumb2 = @item.imageUrl2[0..-5]
+    @thumb2 = @thumb2 + "thumb.jpg"
+    @thumb3 = @item.imageUrl3[0..-5]
+    @thumb3 = @thumb3 + "thumb.jpg"
+    @thumb4 = @item.imageUrl4[0..-5]
+    @thumb4 = @thumb4 + "thumb.jpg"
+    @contact_form = ContactForm.new
   end
   
+  private
+  
+ 
   
 end
